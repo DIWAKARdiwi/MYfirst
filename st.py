3b9cb0667ca5cfe1a0ed8e4b4e7cb5fca54371b8
Diwakar N <diwakardiwi11@gmail.com>
2023-08-02 17:49:58 +0530

    got stash items into program stsr branch

--- st.py
+++ st.py
@@ -1,4 +1,13 @@
 #stashing
 print("Hellow world")
 
+<<<<<<< Updated upstream
 print("Python programming is easy")
+=======
+#stsr branch
+print("Python programming")
+
+print("I am at master branch ")
+
+print("Fortran program")
+>>>>>>> Stashed changes
