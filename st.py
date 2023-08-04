0789a3d9fe6d04190c6d98201a77712e9edbbafc
Diwakar N <diwakardiwi11@gmail.com>
2023-08-04 17:45:41 +0530

    git hub commit

--- st.py
+++ st.py
@@ -10,8 +10,14 @@ print("Python programming")
 print("I am at master branch ")
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 print("Fortran program")
 >>>>>>> Stashed changes
 =======
 print("Fortran program")
 >>>>>>> Stashed changes
+=======
+print("Fortran program")
+>>>>>>> Stashed changes
+
+print("working in git hub")
